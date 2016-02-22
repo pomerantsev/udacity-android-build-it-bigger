fc8517414e7c011678a9bd768bc342b7cafd9a7c
Pavel Pomerantsev <pomerantsevp@gmail.com>
2016-02-22 18:24:33 +0300

    Make the endpoint accept no parameters instead of one

--- app/src/main/java/ru/pomerantsevp/example/testendpoints/MainActivity.java
+++ app/src/main/java/ru/pomerantsevp/example/testendpoints/MainActivity.java
@@ -68,7 +68,7 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
         String name = params[0].second;
 
         try {
-            return myApiService.sayHi(name).execute().getData();
+            return myApiService.sayJoke().execute().getData();
         } catch (IOException e) {
             return e.getMessage();
         }
